30bbbeb64bb79b40f4cc16e494987784b574007d
haikyall1 <haikyallwriter@gmail.com>
2024-10-04 13:11:27 +0800

    Update chat 1004.dart

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:xd/home.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -7,6 +8,45 @@ class ChatPage extends StatefulWidget {
   // ignore: library_private_types_in_public_api
   _ChatPageState createState() => _ChatPageState();
 }
+
+ void _dialogBuilder(BuildContext context) {
+     showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Color(0xFFFA643F),
+          iconColor: Color.fromARGB(0, 255, 155, 130),//ff9d85
+          title: const Text('Pro Version Access!'),
+          content: const Text(
+            'As much as we would love to serve you, you are currently accessing a feature that isn’t part of your current plan.\n'
+            'To change your plan, kindly refer to our Pricing Plans for more information.\n'),
+          actions: <Widget>[
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Pricing Plans'),
+              onPressed: () {   Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ChatPage()),);
+              },
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('back to chat'),
+              onPressed: () {
+                Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ChatPage()),);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
 class _ChatPageState extends State<ChatPage> {
   final List<String> messages = []; // Store chat messages
@@ -21,13 +61,16 @@ class _ChatPageState extends State<ChatPage> {
     }
   }
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF7A5C47), // Background color
       appBar: AppBar(
+        title: Text('Student'), 
         backgroundColor:const Color(0xFF7A5C47),
-        leading: IconButton(onPressed: (){Navigator.pop(context);}, icon: const Icon(Icons.home)),
+        leading: IconButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));}, icon: const Icon(Icons.home)),
       ),
       body: Center(
         child: Container(
@@ -96,6 +139,16 @@ class _ChatPageState extends State<ChatPage> {
                     IconButton(
                       icon: const Icon(Icons.send),
                       onPressed: _sendMessage,
+                      color: Colors.black,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.mic),
+                      onPressed: () => _dialogBuilder(context),
+                      color: Colors.black,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.photo),
+                      onPressed: () => _dialogBuilder(context),
                       color: Colors.black,
                     ),
                   ],
