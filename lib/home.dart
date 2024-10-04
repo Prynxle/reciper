2232c0eca99c8eca267b9df491d7f03fa4d4b6ab
haikyall1 <haikyallwriter@gmail.com>
2024-10-04 13:10:47 +0800

    Update home 1004.dart

--- lib/home.dart
+++ lib/home.dart
@@ -4,10 +4,62 @@ import 'chat.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  Future<void> _dialogBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Color(0xFFFA643F),
+          iconColor: Color.fromARGB(0, 255, 155, 130),//ff9d85
+          title: const Text('Help us better understand you better!'),
+          content: const Text(
+            'Kindly click whichever applies to you:\n'
+            'Are you a...\n'),
+          actions: <Widget>[
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Student'),
+              onPressed: () {   Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ChatPage()),);
+              },
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Working Adult'),
+              onPressed: () {
+                Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ChatPage()),);
+              },
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Parent'),
+              onPressed: () {
+               Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ChatPage()),);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      appBar: AppBar(title: Text('dormEATory'), backgroundColor: Color(0xFFFA643F),),
+      body:
+      Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/home.png'), fit: BoxFit.cover
@@ -25,10 +77,7 @@ class HomePage extends StatelessWidget {
                   ),
                   child: const Text('Open Chat', style: TextStyle(color: Colors.white),),
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const ChatPage()),
-                    );
+                    _dialogBuilder(context);
                   },
                 ),
                 const SizedBox(height: 20), 
